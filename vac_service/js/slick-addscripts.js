e441764b84b1db6965e1cd4b56b827ad04925097
veelatseng <veela.tzeng@gmail.com>
2025-01-22 12:17:59 +0800

    新增slick

---
+++ vac_service/js/slick-addscripts.js
@@ -0,0 +1,53 @@
+/*------底圖*/	
+$(document).ready(function(){
+	
+$('.adbanner').slick({
+	dots: false,//顯示原點
+	speed: 3000,
+	arrows:true, //左右箭頭
+	autoplay:true,//自動播放
+	infinite: true, //無限輪播
+	slidesToShow: 4,
+	slidesToScroll: 4,
+	responsive: [
+		{
+			breakpoint: 980,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4,
+			}
+		},
+		{
+			breakpoint: 690,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3
+			}
+		},
+		{
+			breakpoint: 550,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 420,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	]
+});	
+//click button then pause it.
+$('.slick-number>li,.slick-prev,.slick-next').on('click',function(){
+	$(".bigbanner").slick('slickPause');
+});	
+});
+
+
